ca86fe083f1656567732d62c134d76db9f312a13
srijonsarkar1 <srijon.sarkar@oracle.com>
2022-01-27 11:57:50 +0530

    Train and Eval stage added

---
+++ src/train_and_eval.py
@@ -0,0 +1,60 @@
+import os, warnings, sys, argparse, pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import ElasticNet
+from get_data import read_params
+
+
+def eval_metics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+def train_and_eval(config_path):
+    config = read_params(config_path)
+    test_data_path = config["split_data"]["test_path"]
+    train_data_path = config["split_data"]["train_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dir"]
+
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+
+    target = [config["base"]["target_col"]]
+
+    train = pd.read_csv(train_data_path, sep=",")
+    test = pd.read_csv(test_data_path, sep=",")
+
+    train_y = train[target]
+    test_y = test[target]
+
+    train_x = train.drop(target, axis=1)
+    test_x = test.drop(target, axis=1)
+
+    lr = ElasticNet(
+        alpha = alpha,
+        l1_ratio = l1_ratio,
+        random_state = random_state
+    )
+
+    lr.fit(train_x, train_y)
+    pred_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metics(test_y, pred_qualities)
+
+    print(f"ElasticNet model (alpha={alpha}, l1_ratio={l1_ratio} ):")
+    print(f"RMSE : {rmse}")
+    print(f"MAE : {mae}")
+    print(f"R2 : {r2}")
+
+if __name__=='__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_eval(config_path=parsed_args.config)
+
+
+
